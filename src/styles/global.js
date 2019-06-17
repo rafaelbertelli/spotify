856dff16c06fcfa49e6f271c77b154b9cee14be0
Rafael Bertelli <rafaelbertelli89@gmail.com>
2019-06-17 11:58:44 -0300

    Config mouse global style

--- src/styles/global.js
+++ src/styles/global.js
@@ -15,13 +15,31 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background-color: #181818;
     color: #FFF;
+    cursor: default;
     font-family: 'Montserrat', sans-serif;
     text-rendering: optimizeLegibility !important;
+    user-select: none;
     -moz-osx-font-smoothing: grayscale !important;
     -webkit-font-smoothing: antialiased !important;
   }
 
   a, button {
     cursor: pointer;
+  }
+
+  /**
+  * Scrollbar
+  * @font: https://css-tricks.com/custom-scrollbars-in-webkit/
+  */
+  ::-webkit-scrollbar {
+    width: 9px;
+  }
+  ::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
   }
 `;
