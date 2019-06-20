a822b52b96daea9b11228ee2a551b6f4bd55e2c8
Rafael Bertelli <rafaelbertelli89@gmail.com>
2019-06-20 12:21:26 -0300

    Create Ducks

---
+++ src/store/ducks/playlists.js
@@ -0,0 +1,43 @@
+/** TYPES */
+export const Types = {
+  GET_REQUEST: 'playlist/GET_REQUEST',
+  GET_SUCCESS: 'playlist/GETSUCCESS',
+};
+
+/** REDUCERS */
+const INITIAL_STATE = {
+  data: [],
+  loading: false,
+};
+
+export default function playlist(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case Types.GET_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case Types.GET_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: [action.payload.data],
+      };
+
+    default:
+      return state;
+  }
+}
+
+/** ACTIONS */
+export const Creators = {
+  getPlaylistRequest: () => ({
+    type: Types.GET_REQUEST,
+  }),
+
+  getPlaylistSuccess: data => ({
+    type: Types.GET_SUCCESS,
+    payload: { data },
+  }),
+};
